92058d8f8de292de1367fb579eeca7e0de18db1f
Davshn <davshn@gmail.com>
2025-10-17 18:54:47 -0500

    feat: :sparkles: add cart and products reducers with actions and integrate into store
    - Created products reducer with actions for setProducts, addProduct, updateProduct, removeProduct, and loading/error states. - Integrated both reducers into the Redux store and updated root reducer configuration. - Wired up types, initial state, and action creators for strong typing. - Prepared tests scaffolding for reducers, actions, and selectors.

--- mobileApp/src/stateManagement/store.ts
+++ mobileApp/src/stateManagement/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import productsReducer from './reducers/products.reducer';
+import cartReducer from './reducers/cart.reducer';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
